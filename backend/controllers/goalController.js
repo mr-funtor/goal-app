e51a065137438770424fca3b2cdf44c9a6ebcee1
Olufunto Oyewole <olufunto.oyewole@thebulbafrica.institute>
2022-03-10 12:51:52 +0100

    made the CRUD functionality for the api

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,14 @@
 const asyncHandler= require('express-async-handler');
+const Goal =require('../models/goalModel');
 
 
 //Desc Get goals
 //Route GET /api/goals
 //Access Private
 const getGoals= asyncHandler(async(req,res)=>{
-     res.status(200).json({msg:'Get goals'})
+    const goals = await Goal.find()
+    
+     res.status(200).json(goals)
 })
 
 //Desc Set goal
@@ -17,22 +20,43 @@ const setGoal= asyncHandler(async(req,res)=>{
         throw new Error('Please add a text field')
     }
     
+    const goal = await  Goal.create({
+        text: req.body.text
+    })
     
-     res.status(200).json({msg:'Create goal'})
+     res.status(200).json(goal);
 })
 
 //Desc Update goals
 //Route PUT /api/goals/:id
 //Access Private
 const updateGoal= asyncHandler(async(req,res)=>{
-    res.status(200).json({msg:`updated ${req.params.id} goal`})
+    const goal= await Goal.findById(req.params.id);
+    
+    if(!goal){
+        res.status(400);
+        throw new Error('Goal not found')
+    }
+    
+    const updatedGoal= await Goal.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    
+    res.status(200).json(updatedGoal)
 })
 
 //Desc Delete goals
 //Route DELETE /api/goals/:id
 //Access Private
 const deleteGoal= asyncHandler(async(req,res)=>{
-     res.status(200).json({msg:`Deleted ${req.params.id} goal`})
+    const goal= await Goal.findById(req.params.id);
+    
+    if(!goal){
+        res.status(400);
+        throw new Error('Goal not found')
+    }
+    
+   const theDeleted= await goal.deleteOne({_id:req.params.id});
+    
+    res.status(200).json(theDeleted._id)
 })
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express= require('express');
 const app= express();
 const dotenv= require('dotenv').config();
+const colors=require('colors');
 const {errorHandler}=require('./middleware/errorMiddleware');
+const connectDB= require('./config/db');
+
+//connects to mongoose
+connectDB()
 
 //add middleware
 app.use(express.json());
